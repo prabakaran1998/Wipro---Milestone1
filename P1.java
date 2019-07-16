cbf9d73757bb6fd72e672d140bfaf4d5f0a00340
prabakaran1998 <42084128+prabakaran1998@users.noreply.github.com>
2019-07-16 12:22:21 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+public class P1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0]+" Technologies "+args[1]);  
+
+
+	}
+
+}
