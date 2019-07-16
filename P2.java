22f31f369db347cdf0ea9b38a78c7b9c9d911d59
prabakaran1998 <42084128+prabakaran1998@users.noreply.github.com>
2019-07-16 12:24:01 +0530

    Add files via upload

---
+++ P2.java
@@ -0,0 +1,14 @@
+package Assignments;
+import java.util.*;
+
+public class P2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String str=args[0];
+		System.out.println("welcome "+str);
+
+	}
+
+}
